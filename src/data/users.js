5130b0273565a8d0b708aa68a317ca5765da8b71
Mariusz Domanski <mariusz@domanski.pl>
2021-03-18 23:07:14 +0100

    Users List

---
+++ src/data/users.js
@@ -0,0 +1,37 @@
+export const users = [
+    {
+        name: 'Adam Romański',
+        attendance: '39%',
+        average: '2.3',
+    },
+    {
+        name: 'Krzysztof Batko',
+        attendance: '39%',
+        average: '2.3',
+    },
+    {
+        name: 'Patrycja Gonciarz',
+        attendance: '39%',
+        average: '2.3',
+    },
+    {
+        name: 'Olga Hahn',
+        attendance: '39%',
+        average: '2.3',
+    },
+    {
+        name: 'Paweł Andrzejewski',
+        attendance: '39%',
+        average: '2.3',
+    },
+    {
+        name: 'Szymon Knuth',
+        attendance: '39%',
+        average: '2.3',
+    },
+    {
+        name: 'Krystian Foczpaniak',
+        attendance: '39%',
+        average: '2.3',
+    }
+];
