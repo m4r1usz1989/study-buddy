110b7b98e4335083a9d32a3b4b0e681330a0c3e1
Mariusz Domanski <mariusz@domanski.pl>
2021-03-22 00:13:42 +0100

    styling users list

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { users } from 'data/users';
 import UsersListItem from 'components/UsersListItem/UsersListItem';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    background-color: white;
+    width: 100%;
+    max-width: 500px;
+    padding: 40px 30px;
+    border-radius: 25px;
+    box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.3);
+`;
 
 const UsersList = () => (
-  <div>
-    <ul>
-      {users.map((userData) => (
-        <UsersListItem userData={userData} />
-      ))}
-    </ul>
-  </div>
+    <Wrapper>
+        <ul>
+            {users.map((userData) => (
+                <UsersListItem userData={userData} />
+            ))}
+        </ul>
+    </Wrapper>
 );
 
 export default UsersList;
