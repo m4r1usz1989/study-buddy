ce2148293c5b86c656a044d55c881c8108d7be00
quinrix <quinrix@gmail.com>
2021-04-29 13:10:47 +0200

    final solutions

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '../../atoms/Button/Button';
-import { Wrapper, StyledAverage, StyledInfo } from './UsersListItem.styles';
-const UsersListItem = ({ deleteuser, userData: { average, name, attendance } }) => (
-    <Wrapper>
-        <StyledAverage value={average}>{average}</StyledAverage>
-        <StyledInfo>
-            <p>{name}</p>
-            <p>attendance: {attendance}</p>
-        </StyledInfo>
-        <Button onClick={() => deleteuser(name)} />
-    </Wrapper>
+import DeleteButton from 'components/atoms/DeleteButton/DeleteButton';
+import { StyledAverage, StyledInfo, Wrapper } from './UsersListItem.styles';
+
+const UsersListItem = ({ deleteUser, userData: { average, name, attendance = '0%' } }) => (
+  <Wrapper>
+    <StyledAverage value={average}>{average}</StyledAverage>
+    <StyledInfo>
+      <p>
+        {name}
+        <DeleteButton onClick={() => deleteUser(name)} />
+      </p>
+      <p>attendance: {attendance}</p>
+    </StyledInfo>
+  </Wrapper>
 );
 
 UsersListItem.propTypes = {
-    userData: PropTypes.shape({
-        average: PropTypes.string,
-        name: PropTypes.string.isRequired,
-        attendance: PropTypes.string,
-    }),
+  userData: PropTypes.shape({
+    average: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    attendance: PropTypes.string,
+  }),
 };
 
 export default UsersListItem;
